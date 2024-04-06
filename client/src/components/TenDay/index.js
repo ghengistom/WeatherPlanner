bf6a626820082e596862d747288e3d651c7e187c
ghengistom <tomedwardbernard@gmail.com>
2024-04-05 19:04:21 -0700

    Update index.js
    Setting api endpoints with gps coordinates grabbed from the browser versus hard coding it.

--- client/src/components/TenDay/index.js
+++ client/src/components/TenDay/index.js
@@ -4,28 +4,30 @@ import axios from 'axios';
 
 function TenDay() {
 
-  // const x = document.getElementById("div1");
-
-  // function getLocation() {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(showPosition);
-  //   } else {
-  //     x.innerHTML = "Geolocation is not supported by this browser.";
-  //   }
-  // }
-  
-  // function showPosition(position) {
-  //   x.innerHTML = "Latitude: " + position.coords.latitude +
-  //   "<br>Longitude: " + position.coords.longitude;
-  // }
-
-
-
   const [count, setCount] = useState(0);
   const [posts, setPosts] = useState([]);
   const [weatherRecords, setWeatherRecords] = useState([]);
   const [periodRecords, setPeriodRecords] = useState([]);
+  const [gpsX, setGpsX] = useState(null);
+  const [gpsY, setGpsY] = useState(null);
+  const [tenDayApi, setTenDayApi] = useState(null);
 
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      return true;
+    } else {
+       alert("Geolocation is not supported by this browser.");
+       return false;
+    }
+  }
+  function showPosition(position) {
+    const gpsX = position.coords.latitude.toFixed(4);
+    const gpsY = position.coords.longitude.toFixed(4);
+    setGpsX(gpsX); 
+    setGpsY(gpsY);
+    getEndPoints(gpsX, gpsY);
+  }
   const makePage = (array) => {
     array.forEach((element, index, array) => {
       var div = document.createElement('div');
@@ -38,42 +40,59 @@ function TenDay() {
                 <p class="card-text">Temperature: ${element.temperature} ${element.temperatureUnit}</p>
                 <p class="card-text">Wind direction: ${element.windDirection}</p>
                 <p class="card-text">Wind speed: ${element.windSpeed}</p>
-
             </div>
       `;
       document.getElementById('div1').appendChild(div);
-      // console.log(element.name); // 100, 200, 300
-      // console.log(index); // 0, 1, 2
-      // console.log(array); // same myArray object 3 times
     });
   };
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
+  const getEndPoints = (x,y)=>{
     // Update the document title using the browser API    
-    axios.get('https://api.weather.gov/gridpoints/SGX/60,52/forecast')
+    // axios.get('https://api.weather.gov/gridpoints/SGX/60,52/forecast')
+    axios.get(`https://api.weather.gov/points/${x},${y}`)
+    .then(response => {
+      console.log('getEndPoints response.data.properties.forecast', response.data.properties.forecast);
+      setTenDayApi(response.data.properties.forecast)
+      return true;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  const getTenDay = (endpoint)=>{
+    // Update the document title using the browser API    
+    // axios.get('https://api.weather.gov/gridpoints/SGX/60,52/forecast')
+    axios.get(endpoint)
     .then(response => {
       const periodRecords = [...response.data.properties.periods]
       setPeriodRecords(periodRecords);
       setWeatherRecords(response.data.properties);
       setPosts(response.data.properties.periods[0].detailedForecast);
- 
       console.log('periodRecords', periodRecords);
-
       console.log('response.data.properties', response.data.properties);
       console.log('response.data.properties.periods[0].detailedForecast', response.data.properties.periods[0].detailedForecast);
       console.log('response.data.properties', response.data.properties);
-
+      return true;
     })
     .catch(error => {
       console.error(error);
     });
+  }
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    getLocation();
+      console.log('tenDayApi', tenDayApi)
+      if(tenDayApi){
+        getTenDay(tenDayApi);
+      }
+    
+    console.log('gpsX and gpsY' + gpsX + gpsY)
 
-  },[posts]);
+  },[posts, tenDayApi]);
 
   if(posts){
     makePage(periodRecords);
     // makePage(weatherRecords);
-
   }  
 
   return (
@@ -85,8 +104,6 @@ function TenDay() {
       </button> */}
       <div id="div1">
       </div>
-
-      
     </div>
   
 
